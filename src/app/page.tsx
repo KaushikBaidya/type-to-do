8f871a753888a2d4eae24ee75285629138e9ae43
KAUSHIK BAIDYA <47146123+KaushikBaidya@users.noreply.github.com>
2023-09-16 10:59:52 +0600

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { AddTodo } from "@/components/addtodo";
+
 export default function Home() {
   return (
     <main>
       <div>
         <h1>To Do List</h1>
       </div>
+      <AddTodo />
     </main>
   );
 }
--- src/components/addtodo.tsx
+++ src/components/addtodo.tsx
@@ -1,5 +1,24 @@
-import React from "react";
+"use client";
+import react, { FormEvent, useState } from "react";
 
 export const AddTodo = () => {
-  return <div>addtodo</div>;
+  const [todo, setTodo] = useState("");
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          placeholder="Write your to do...."
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button type="submit" onClick={handleSubmit}>
+          Add
+        </button>
+      </form>
+    </div>
+  );
 };
